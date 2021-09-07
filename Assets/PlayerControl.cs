a5ef47ddd8c7c7672814247ebc1736c0acba8080
yazedhaidar <88135665+yazedhaidar@users.noreply.github.com>
2021-09-07 23:35:54 +0700

    Final Pong

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{   // Tombol untuk menggerakkan ke atas
+    public KeyCode upButton = KeyCode.W;
+
+    // tombol untuk menggerakkan ke bawah
+    public KeyCode downButton = KeyCode.S;
+
+    //Kecepatan Gerak
+    public float speed = 10.0f;
+
+    //Batas atas dan bawah game scene (Batas bawah menggunakan (-))
+    public float yBoundary = 9.0f;
+
+    //Rigidbody 2D untuk raket ini
+    private Rigidbody2D rigidbody2D;
+
+    // Titik tumbukan terakhir dengan bola, untuk menampilkan variabel-variabel fisika terkait tumbukan tersebut
+    private ContactPoint2D lastContactPoint;
+
+    // Skor Pemain 
+    private int score;        
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Dapatkan Kecepatan raket Sekarang
+        Vector2 velocity = rigidbody2D.velocity;
+
+        //jika pemain menekan tombol ke atas, beri kecepatan positif ke komponen y (ke atas)
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+
+        // Jika pemain menekan tombol ke bawah, beri kecepatan negatif ke komponen y (ke bawah).
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+
+        // Jika pemain tidak menekan tombol apa-apa, kecepatannya nol.
+        else
+        {
+            velocity.y = 0.0f;
+        }
+
+        // Masukkan kembali kecepatannya ke rigidBody2D.
+        rigidbody2D.velocity = velocity;
+
+        // Dapatkan posisi raket sekarang.
+        Vector3 position = transform.position;
+
+        // Jika posisi raket melewati batas atas (yBoundary), kembalikan ke batas atas tersebut.
+        if (position.y > yBoundary)
+        {
+            position.y = yBoundary;
+        }
+
+        // Jika posisi raket melewati batas bawah (-yBoundary), kembalikan ke batas atas tersebut.
+        else if (position.y < -yBoundary)
+        {
+            position.y = -yBoundary;
+        }
+
+        // Masukkan kembali posisinya ke transform.
+        transform.position = position;
+        
+    }
+
+    // Menaikkan skor sebanyak 1 poin
+    public void IncrementScore()
+    {
+        score++;
+    }
+
+    // Mengembalikan skor menjadi 0
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    // Mendapatkan nilai skor
+    public int Score
+    {
+        get { return score; }
+    }
+
+    // Untuk mengakses informasi titik kontak dari kelas lain
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+
+    // Ketika terjadi tumbukan dengan bola, rekam titik kontaknya.
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Ball"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }
+    }
+}
